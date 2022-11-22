9fd98beef346e02812024a4a1276fdd1d3c295c0
Isuru Yahampath <isuruyahampath1@gmail.com>
2022-11-22 11:44:02 +0530

    finished encapsulation

--- Encapsulation/Encapsulation/Program.cs
+++ Encapsulation/Encapsulation/Program.cs
@@ -11,22 +11,29 @@ namespace Encapsulation
         int speed = 0, gear = 0; // state (attribute)
         string color; 
 
-        void SpeedUp(int a) //behaviours (method)
+        public void SpeedUp(int a) //behaviours (method)
         {
             speed += a;
+
             Console.WriteLine("Current speed: " + speed);
         }
 
-        void SpeedDown(int a)
+        public void SpeedDown(int a)
         {
             speed -= a;
             Console.WriteLine("Current speed: " + speed);
 
         }
 
-        void GearUp(int a)
+        public void GearUp(int a)
         {
             gear += a;
+            Console.WriteLine("Current gear: " + speed);
+
+        }
+        public void GearDown(int a)
+        {
+            gear -= a;
             Console.WriteLine("Current gear: " + speed);
 
         }
@@ -36,7 +43,10 @@ namespace Encapsulation
     {
         static void Main(string[] args)
         {
+            Pulsar2000 bike1 = new Pulsar2000();
+            bike1.SpeedUp(1);
+
             Console.ReadKey();
         }
-    }   
+    }
 }
