24b8ef5812d015fcacb6ec5f3c2b225966ac5be8
Isuru Yahampath <isuruyahampath1@gmail.com>
2022-12-05 10:58:24 +0530

    added inheritance

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Father
+    {
+        public string house = "two-storey";
+        public string car = "sedan";
+        string designation = "Director";
+
+    }
+
+    class Child : Father //inheritance
+    {
+        public void display()
+        {
+            Console.WriteLine("This is my car: " + car);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Child child = new Child();
+            child.display();
+        }
+    }
+}
