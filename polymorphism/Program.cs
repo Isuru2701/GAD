0ad7f8f88de2bfc396a98aad2b0b6d83bacd7fcc
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-01-03 10:45:55 +0530

    added polymorphism

---
+++ polymorphism/Program.cs
@@ -0,0 +1,35 @@
+using System;
+namespace Polymorphism
+{
+    class Human
+    {
+        public void eat()
+        {
+            Console.WriteLine("Person ate no meal today");
+        }
+        public bool eat(string meal1)
+        {
+            Console.WriteLine("Person ate 1 meal today");
+        }
+        public void eat(string meal1, string meal2)
+        {
+            Console.WriteLine("Person ate 2 meals today");
+        }
+        public void eat(string meal1, string meal2, string meal3)
+        {
+            Console.WriteLine("Person ate 3 meals today");
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args) { 
+        
+        Human human = new Human();
+        human.eat();
+        
+        }
+    }
+      
+}
+
---
+++ polymorphism/polymorphism/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+namespace Polymorphism
+{
+    class Human
+    {
+        public void eat()
+        {
+            Console.WriteLine("Person ate no meal today");
+        }
+        public bool eat(string meal1)
+        {
+            Console.WriteLine("Person ate 1 meal today");
+            return true;
+        }
+        public void eat(string meal1, string meal2)
+        {
+            Console.WriteLine("Person ate 2 meals today");
+        }
+        public void eat(string meal1, string meal2, string meal3)
+        {
+            Console.WriteLine("Person ate 3 meals today");
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Human human = new Human();
+            human.eat();
+            Console.Read();
+        }
+    }
+
+}
+
+
