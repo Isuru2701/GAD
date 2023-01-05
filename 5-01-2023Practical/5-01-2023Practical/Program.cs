560d8a104bc1a25e0c2f08a4f04c12c064feb6a8
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-01-05 09:10:05 +0530

    first for the day

---
+++ 5-01-2023Practical/5-01-2023Practical/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_01_2023Practical
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
