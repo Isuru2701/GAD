3f9ffcf6a0af86c65bb0fde1b3ee9c81d14a58c3
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-01-05 13:08:03 +0530

    kill me please

---
+++ 5-01-2023Practical/inheritane_exercise/parent.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritane_exercise
+{
+    class Father
+    {
+        public string house = "12/31 Rds.HQufaf";
+        protected string carModel = "idgaf";
+        internal string surname = "fml";
+        private double account_balance = 69420;
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine("House: " + house + "\ncar model: " + carModel + "\nsurname: " + surname + "\naccount balance: " + account_balance);
+        }
+    }
+
+    class Child : Father
+    {
+        public string degree = "kms", name = "fakwgfwau";
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine("House: " + house + "\ncar model: " + carModel + "\nname: " + name + "\nsurname: " + surname + "\ndegree: " + degree);
+        }
+    }
+    internal class parent
+    {
+        static void Main(string[] args)
+        {
+            Child child = new Child();
+            child.DisplayDetails();
+            Console.ReadKey();
+        }
+    }
+}
