efe54e2d806152bad3e7cbfee606c741dca2d718
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-01-05 10:00:31 +0530

    added stuf

--- 5-01-2023Practical/5-01-2023Practical/Program.cs
+++ 5-01-2023Practical/5-01-2023Practical/Program.cs
@@ -10,7 +10,58 @@ namespace _5_01_2023Practical
     {
         static void Main(string[] args)
         {
+            Calculator();
+            Console.ReadKey();
 
+        }
+
+        static void Parity()
+        {
+            int number;
+            Console.WriteLine("Enter number: ");
+            number = Console.Read();
+            if (number % 2 != 0)
+            {
+                Console.WriteLine("number is odd");
+            }
+            else
+            {
+                Console.WriteLine("number is even");
+            }
+        }
+
+        static void Calculator() 
+        {
+            double num1, num2, num3;
+            int op;
+            Console.WriteLine("Enter number 1");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter number 2");
+            num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter number 3");
+            num3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter operation: \n1. addition\n2. substraction\n3. multiplication");
+            op = Convert.ToInt16(Console.ReadLine());
+
+            switch(op)
+            {
+                case 1:
+                    Console.WriteLine("addition = " + (num1 + num2 + num3));
+                    break;
+
+                case 2:
+                    Console.WriteLine("substraction = " + (num1 - num2 - num3));
+                    break;
+
+                case 3:
+                    Console.WriteLine("multiplcation = " + (num1 * num2 * num3));
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid input");
+                    break;
+            }
         }
     }
 }
