4b45a3d80dd7464366c96a6a17052cd6e9c15b58
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-01-09 09:30:01 +0530

    abstract class Vehicle added

--- Abstract classes/Abstract classes/Program.cs
+++ Abstract classes/Abstract classes/Program.cs
@@ -6,10 +6,42 @@ using System.Threading.Tasks;
 
 namespace Abstract_classes
 {
+    abstract class Vehicle
+    {
+        public abstract void SpeedUp();
+        public abstract void Brake();
+        public abstract void ChangeGear();
+
+    }
+
+    class Car : Vehicle
+    {
+        public Car()
+        {
+
+        }
+
+        public override void SpeedUp()
+        {
+
+        }
+        public override void Brake() 
+        { 
+        
+        }
+        public override void ChangeGear()
+        {
+            
+        }
+
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+
         }
+
+       
     }
 }
