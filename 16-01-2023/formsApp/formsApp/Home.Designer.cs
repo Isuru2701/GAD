7aa05d8c3d122acdfe95997d7600091b915f6bb5
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-01-16 08:52:50 +0530

    initial for the day

---
+++ 16-01-2023/formsApp/formsApp/Home.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace formsApp
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_display = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_display
+            // 
+            this.btn_display.Font = new System.Drawing.Font("JetBrains Mono", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_display.Location = new System.Drawing.Point(335, 184);
+            this.btn_display.Name = "btn_display";
+            this.btn_display.Size = new System.Drawing.Size(108, 69);
+            this.btn_display.TabIndex = 0;
+            this.btn_display.Text = "Display";
+            this.btn_display.UseVisualStyleBackColor = true;
+            this.btn_display.Click += new System.EventHandler(this.btn_display_Click);
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_display);
+            this.Name = "Home";
+            this.Text = "Home";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_display;
+    }
+}
+
