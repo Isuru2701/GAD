2a89926446169fb665dd7762dd311baa6de1e2e2
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-02-03 12:00:00 +0530

    added sqlcommand object

--- database/database/Registration.xaml.cs
+++ database/database/Registration.xaml.cs
@@ -33,5 +33,39 @@ namespace database
         {
             con = new SqlConnection("Data Source=ISURU;Initial Catalog=Bank;Integrated Security=True");
         }
+
+        private void btn_save_Click(object sender, RoutedEventArgs e)
+        {
+                con.Open();
+
+            //string interpolation is better here
+
+            cmd = new SqlCommand("Insert into Client Values('" +txt_id.Text +"','"+
+                txt_name.Text+"','"+txt_address.Text+
+                "', '"+picker_dob.DisplayDate+
+                "', '"+txt_age.Text+"'," +
+                "'"+txt_phone.Text+"')", con);
+
+                //the painful way:
+                //cmd = new SqlCommand("INSERT INTO BANK VALUES (@a, @b, @c, @d, @e, @f)", con);
+
+                //cmd.Parameters.AddWithValue("a", txt_id.Text);
+                //cmd.Parameters.AddWithValue("b", txt_name.Text);
+                //cmd.Parameters.AddWithValue("c", txt_address.Text);
+                //cmd.Parameters.AddWithValue("d", picker_dob.DisplayDate);
+                //cmd.Parameters.AddWithValue("e", Convert.ToInt32(txt_age.Text));
+                //cmd.Parameters.AddWithValue("f", Convert.ToInt32(txt_phone.Text));
+
+                if(cmd.ExecuteNonQuery() == 1)
+                {
+                    MessageBox.Show("Data Entry Successful");
+                }
+                else
+            {
+                MessageBox.Show("ENTRY ERROR");
+            }
+
+                con.Close();
+        }
     }
 }
